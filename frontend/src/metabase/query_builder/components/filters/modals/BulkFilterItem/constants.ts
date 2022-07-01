412e9b0755e30257811e9f1f0d898f29fd0b7abd
Ryan Laurie <iethree@gmail.com>
2022-07-01 11:59:35 -0600

    WIP v2 redesign

--- frontend/src/metabase/query_builder/components/filters/modals/BulkFilterItem/constants.ts
+++ frontend/src/metabase/query_builder/components/filters/modals/BulkFilterItem/constants.ts
@@ -1,4 +1,6 @@
 export const FIELD_PRIORITY = [
+  "type/Boolean",
+  "type/Date",
   "type/DateTime",
   "type/DateTimeWithTZ",
   "type/DateTimeWithLocalTZ",
@@ -7,7 +9,6 @@ export const FIELD_PRIORITY = [
   "list",
   "type/Integer",
   "type/Float",
-  "type/Boolean",
   "type/Category",
   "type/PK",
   "type/FK",
